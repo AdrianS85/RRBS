536430520a70325c9f4dfd743659153f64717b50
AdrianS85 <adrianstankiewicz85@gmail.com>
2018-10-01 10:38:02 +0200

    Update RRBS_follow_up.sh

--- RRBS_follow_up.sh
+++ RRBS_follow_up.sh
@@ -3,9 +3,13 @@ export FASTQ_DEDUP_FOL=/tmp/
 
 cd Bismark &&
 
+#FROM INSIDE THE CONTAINER?
 ls *stripped.bam | sort >> nudup.txt &&
 ls $FASTQ_DEDUP_FOL*_R2_* | sort >> fq2.txt &&
+# paste fq2.txt nudup.txt >> pairs
 parallel --verbose --link --joblog jolog.txt --memfree 20 --retries 10 --delay 1 --tmpdir /tmp/tmp/ --jobs 10  "python /nugentechnologies-nudup-7a126eb/nudup.py --rmdup-only  -T /tmp/tmp/ --paired-end -f {1} -o {2.} {2}" :::: fq2.txt nudup.txt &> nudup_raport.txt
+#FROM OUTSIDE THE CONTAINER?
+#parallel --verbose --joblog jolog.txt --colsep '\t' "singularity exec --cleanenv --bind ./Bind:/tmp --pwd /tmp/Analysis/Bismark --writable RRBS_Singularity_New.simg python /nudup-master/nudup.py --rmdup-only  --paired-end -f {1} -o {2.} {2}" :::: Bind/Analysis/Bismark/pairs &> Bind/Analysis/Bismark/nudup_raport.txt
 
 parallel "samtools sort -n -o {.}.sorted.bam {}" ::: *sorted.dedup.bam &> sam2_raport.txt &&
 
@@ -19,3 +23,5 @@ bismark2report &&
 bismark2summary &&
 multiqc -n Bismark_multiqc.html . &&
 rm -R ../work/
+
+#
