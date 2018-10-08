e53f862c98403bfaa2cb6eab41533fa6d7d51e51
AdrianS85 <adrianstankiewicz85@gmail.com>
2018-10-08 10:20:53 +0200

    Update RRBS_follow_up_dedup.sh

--- RRBS_follow_up_dedup.sh
+++ RRBS_follow_up_dedup.sh
@@ -1,17 +1,14 @@
 ### This adress is needed to 
-export FASTQ_DEDUP_FOL=/tmp/
+export FASTQ_DEDUP_FOL=/tmp/Analysis/ &&
 
 cd Bismark &&
 
 #FROM INSIDE THE CONTAINER?
 ls *stripped.bam | sort >> nudup.txt &&
 ls $FASTQ_DEDUP_FOL*_R2_* | sort >> fq2.txt &&
-# paste fq2.txt nudup.txt >> pairs
-parallel --verbose --link --joblog jolog.txt --memfree 20 --retries 10 --delay 1 --tmpdir /tmp/tmp/ --jobs 10  "python /nugentechnologies-nudup-7a126eb/nudup.py --rmdup-only  -T /tmp/tmp/ --paired-end -f {1} -o {2.} {2}" :::: fq2.txt nudup.txt &> nudup_raport.txt
-#FROM OUTSIDE THE CONTAINER?
-#parallel --verbose --joblog jolog.txt --colsep '\t' "singularity exec --cleanenv --bind ./Bind:/tmp --pwd /tmp/Analysis/Bismark --writable RRBS_Singularity_New.simg python /nudup-master/nudup.py --rmdup-only  --paired-end -f {1} -o {2.} {2}" :::: Bind/Analysis/Bismark/pairs &> Bind/Analysis/Bismark/nudup_raport.txt
+paste fq2.txt nudup.txt >> pairs
 
-#sudo parallel --verbose --joblog jolog.txt --colsep '\t' "singularity exec --cleanenv --bind ./Analysis:/tmp --pwd /tmp/Bismark2 --writable SN1.simg python /nugentechnologies-nudup-7a126eb/nudup.py --rmdup-only  --paired-end -f {1} -o {2.} {2}" :::: pairs | sudo tee nudup_raport.txt
+parallel --verbose --link --joblog jolog.txt--tmpdir /tmp/tmp/ --jobs 5  "python /nugentechnologies-nudup-7a126eb/nudup.py --rmdup-only  -T /tmp/tmp/ --paired-end -f {1} -o {2.} {2}" :::: pairs &> nudup_raport.txt
 
 parallel "samtools sort -n -o {.}.sorted.bam {}" ::: *sorted.dedup.bam &> sam2_raport.txt &&
 
