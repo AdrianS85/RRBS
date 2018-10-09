8f0eaf3ed276a24c5377e04407eccbd361a29b5a
AdrianS85 <adrianstankiewicz85@gmail.com>
2018-10-09 14:51:02 +0200

    Update RRBS_follow_up_dedup.sh

--- RRBS_follow_up_dedup.sh
+++ RRBS_follow_up_dedup.sh
@@ -13,12 +13,12 @@ cat nudup_raport.txt >> nudup_raport_old.txt &&
 #FROM INSIDE THE CONTAINER?
 ls *stripped.sam | sort > nudup.txt &&
 ls $FASTQ_DEDUP_FOL*_R2_* | sort > fq2.txt &&
-paste fq2.txt nudup.txt > pairs.txt
+paste fq2.txt nudup.txt > pairs.txt &&
 
 ls *stripped.sorted.dedup.bam | sed 's/_R1_.*//' > already_done.txt &&
 grep -v -f already_done.txt pairs.txt > pairs_to_do.txt &&
 
-parallel --verbose --joblog jolog.txt --tmpdir /tmp/ --jobs 5 --colsep "\t" "python /nudup-master/nudup.py --rmdup-only  -T /tmp/ --paired-end -f {1} -o {2.} {2}" :::: pairs_to_do.txt &> nudup_raport.txt
+parallel --verbose --joblog jolog.txt --tmpdir /tmp/ --jobs 5 --colsep "\t" "python /nudup-master/nudup.py --rmdup-only  -T /tmp/ --paired-end -f {1} -o {2.} {2}" :::: pairs_to_do.txt &> nudup_raport.txt &&
 
 parallel "samtools sort -n -o {.}.sorted.bam {}" ::: *sorted.dedup.bam &> sam2_raport.txt &&
 
